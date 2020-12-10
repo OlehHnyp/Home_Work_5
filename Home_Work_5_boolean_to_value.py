ba8f50d69b1aabf7d38640145df1e158a28519d0
Oleh Hnyp <lefk6230@gmail.com>
2020-12-10 19:06:39 +0200

    Add last three tasks

---
+++ Home_Work_5_boolean_to_value.py
@@ -0,0 +1,9 @@
+def bool_to_word(boolean):
+    """
+    This function return 'Yes' if
+    get True and 'No' if get False
+    Input paramemers: boolean
+    Output: 'Yes' or 'No'
+    """
+
+    return 'Yes' if boolean else 'No'
---
+++ Home_Work_5_counting_sheep.py
@@ -0,0 +1,11 @@
+def count_sheeps(sheep):
+    """
+    This function count True
+    in sheep
+    Parameters: sheep
+    Output: int
+    """
+
+    return sheep.count(True)
+
+    
